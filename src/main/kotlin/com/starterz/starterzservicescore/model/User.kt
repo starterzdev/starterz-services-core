86767bf9c7b877061bd396d698e0a45c7e94e77c
ro.stradamus <rolee0429@gmail.com>
2021-07-16 01:24:20 -0400

    STAR-9: Implement basic(auth) user model with DB table

---
+++ src/main/kotlin/com/starterz/starterzservicescore/model/User.kt
@@ -0,0 +1,21 @@
+package com.starterz.starterzservicescore.model
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.relational.core.mapping.Table
+import java.time.LocalDateTime
+
+@Table("users")
+data class User(
+    @Id
+    val id: Long? = null,
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    @CreatedDate
+    val createdAt: LocalDateTime,
+    @LastModifiedDate
+    val updatedAt: LocalDateTime,
+    val version: Long,
+)
