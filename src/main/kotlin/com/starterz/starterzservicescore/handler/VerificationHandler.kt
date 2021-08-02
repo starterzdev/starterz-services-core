d9fb798c3afc5e6df92653343f7b42cae99933f5
ro.stradamus <rolee0429@gmail.com>
2021-08-01 22:25:12 -0400

    STAR-27: Implement verification rediretion instead of using plain REST API endpoint

--- src/main/kotlin/com/starterz/starterzservicescore/handler/VerificationHandler.kt
+++ src/main/kotlin/com/starterz/starterzservicescore/handler/VerificationHandler.kt
@@ -1,6 +1,5 @@
 package com.starterz.starterzservicescore.handler
 
-import com.auth0.jwt.exceptions.JWTVerificationException
 import com.starterz.starterzservicescore.service.JwtService
 import com.starterz.starterzservicescore.service.UserService
 import com.starterz.starterzservicescore.service.exception.AlreadyIntegratedException
@@ -9,6 +8,7 @@ import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
+import java.net.URI
 
 @Component
 class VerificationHandler(
@@ -22,13 +22,13 @@ class VerificationHandler(
                 userService.updateKakaoConnectionId(verificationPayload.userId, verificationPayload.connectionId) }
             .flatMap { user ->
                 val models = mapOf("verifiedUser" to user)
-                ServerResponse.ok().contentType(MediaType.TEXT_HTML).render("verification/complete", models)
+                ServerResponse.temporaryRedirect(URI.create("/web/verification/complete")).build()
             }
             .onErrorResume(AlreadyIntegratedException::class.java) { buildExpiredPageResponse() }
-            .onErrorResume(JWTVerificationException::class.java) { buildExpiredPageResponse() }
             .onErrorResume { buildFailedPageResponse() }
     }
-
-    private fun buildExpiredPageResponse() = ServerResponse.ok().contentType(MediaType.TEXT_HTML).render("verification/expired");
-    private fun buildFailedPageResponse() = ServerResponse.ok().contentType(MediaType.TEXT_HTML).render("verification/failed");
+    private fun buildExpiredPageResponse() = ServerResponse.temporaryRedirect(URI.create("/web/verification/expired")).build()
+//    private fun buildExpiredPageResponse() = ServerResponse.ok().contentType(MediaType.TEXT_HTML).render("verification/expired")
+//    private fun buildFailedPageResponse() = ServerResponse.ok().contentType(MediaType.TEXT_HTML).render("verification/failed")
+    private fun buildFailedPageResponse() = ServerResponse.temporaryRedirect(URI.create("/web/verification/failed")).build()
 }
--- src/main/kotlin/com/starterz/starterzservicescore/router/VerificationRouter.kt
+++ src/main/kotlin/com/starterz/starterzservicescore/router/VerificationRouter.kt
@@ -3,12 +3,15 @@ package com.starterz.starterzservicescore.router
 import com.starterz.starterzservicescore.handler.VerificationHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.server.router
 
 @Configuration
 class VerificationRouter(private val verificationHandler: VerificationHandler) {
     @Bean
     fun verificationRoutes() = router {
-        GET("/verification/user/{token}", verificationHandler::verifyUserIntegration)
+        (accept(MediaType.APPLICATION_JSON) and "/api/v1").nest {
+            GET("/verification/user/{token}", verificationHandler::verifyUserIntegration)
+        }
     }
 }
